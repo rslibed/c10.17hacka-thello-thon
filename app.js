78b7dadc753dae8b58368b078d893303c4a72362
rslibed <rslibed@gmail.com>
2017-11-15 12:09:05 -0800

    Dynamically created game board and added cells/empty classes

--- app.js
+++ app.js
@@ -1,34 +1,58 @@
-var states = {
-    player1: function () {
-
-        // Check adjacent piece for valid move?
-        function checkNeighbors() {
-
+$(document).ready(initializeApp);
+function initializeApp () {
+    var createGame = new Othello();
+    $(".cell").click(createGame.placePiece.bind(createGame));
+    createGame.createGameBoard();
+}
+function Othello () {
+    this.createGameBoard = function () {
+        for (var i = 0; i < 4; i++) {
+            var row = $("<div>").addClass("row");
+                $(".board-container").append(row);
         }
-
-        // Validity = true, place piece
-        function placePiece() {
-
+        for (var j = 0; j < 4; j++) {
+            var cell = $("<div>").addClass("cell").addClass("empty");
+            $(".row").append(cell);
         }
-        // Find piece until matches player's piece? Then flip all opposite stones in btwn
-        function flipPiece() {
-
-        }
-
-    },
-
-    player2: function () {
-        function checkNeighbors() {
-
-        }
-
-        function placePiece() {
-
-        }
-
-        function flipPiece() {
-
-        }
-
     }
-};
+    this.gamePiece = $("<div>").addClass("piece").addClass("black");
+    this.placePiece = function () {
+        if ($(event.target).hasClass("empty")) {
+            $(event.target).append(this.gamePiece).removeClass("empty");
+        }
+    }
+}
+// var states = {
+//     player1: function () {
+//
+//     // Check adjacent piece for valid move?
+//     function checkNeighbors() {
+//
+//     }
+//
+//     // Validity = true, place piece
+//     function placePiece() {
+//
+//     }
+//     // Find piece until matches player's piece? Then flip all opposite stones in btwn
+//     function flipPiece() {
+//
+//     }
+//
+// },
+//
+// player2: function () {
+//     function checkNeighbors() {
+//
+//     }
+//
+//     function placePiece() {
+//
+//     }
+//
+//     function flipPiece() {
+//
+//     }
+//
+// }
+// };
---
+++ cells.js
@@ -0,0 +1,22 @@
+// function findEmptyCells(){
+//     var indicesOfEmpty = [];
+//     var index = rowArray.indexOf(empty);
+//     while (index !== -1){
+//         indicesOfEmpty.push(index);
+//         index = rowArray.indexOf(empty, index + 1);
+//     }
+//     console.log(indicesOfEmpty);
+// }
+// findEmptyCells();
+// var empty = $('.empty');  //This is connected to nothing.
+
+//For player black
+//
+// function findLegalMoveInRow(player){
+//     if (rowArray[index-1] === undefined || rowArray[index-1] === player || rowArray[index-1] === empty) {
+//         break;
+//     } else {
+//         i = rowArray[index]
+//     }
+//
+// }
