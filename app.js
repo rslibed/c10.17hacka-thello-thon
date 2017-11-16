cedad8218a6bced3e04de3ec176f0813a2c3e108
rslibed <rslibed@gmail.com>
2017-11-15 17:25:59 -0800

    Developed alternated turns between players and applied click handlers to game board

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ function initializeApp() {
     var createGame = new Othello();
     createGame.createGameBoard();
     createGame.initialPieces();
+    // $(".cell").click(createGame.currentTurn.placePiece);
     $(".cell").click(createGame.placePiece);
 }
 
 function Othello() {
+    var self = this;
     this.createGameBoard = function () {
         for (var i = 0; i < 8; i++) {
             gameboard[i] = new Array(8);
@@ -22,64 +24,81 @@ function Othello() {
                 gameboard[i][j].appendTo(".board-container");
             }
         }
-    }
-
+    };
     this.initialPieces = function () {
         gameboard[3][3].append($('<div>', {
             'class': 'piece white'
-        }));
+        })).removeClass("empty");
         gameboard[3][4].append($('<div>', {
             'class': 'piece black'
-        }));
+        })).removeClass("empty");
         gameboard[4][3].append($('<div>', {
             'class': 'piece black'
-        }));
+        })).removeClass("empty");
         gameboard[4][4].append($('<div>', {
             'class': 'piece white'
-        }));
-    }
+        })).removeClass("empty");
+    };
+
+    this.currentPlayer = "black";
+
     this.placePiece = function () {
         if ($(event.target).hasClass("empty")) {
             $(event.target).append($('<div>', {
-                'class': 'piece black'
-            }));
+                'class': 'piece ' + self.currentPlayer
+            })).removeClass("empty");
+
+        }
+        if (self.currentPlayer === "black") {
+            self.currentPlayer = "white";
+        } else {
+            self.currentPlayer = "black";
         }
     }
+    // this.states = {
+    //     player1: {
+    //         name: "player 1",
+    //         // Check adjacent piece for valid move?
+    //         checkNeighbors: function () {
+    //             console.log("Check Neighbors.");
+    //         },
+    //         // Validity = true, place piece
+    //         placePiece: function () {
+    //                 if ($(event.target).hasClass("empty")) {
+    //                     $(event.target).append($('<div>', {
+    //                         'class': 'piece black'
+    //                     })).removeClass("empty");
+    //
+    //                 }
+    //             self.currentTurn = self.states.player2;
+    //             },
+    //         // Find piece until matches player's piece? Then flip all opposite stones in btwn
+    //         flipPiece: function () {
+    //             console.log("Flip piece.")
+    //         }
+    //     },
+    //     player2: {
+    //         name: "player 2",
+    //         // Check adjacent piece for valid move?
+    //         checkNeighbors: function () {
+    //             console.log("Check Neighbors.");
+    //         },
+    //         // Validity = true, place piece
+    //         placePiece: function () {
+    //                 if ($(event.target).hasClass("empty")) {
+    //                     $(event.target).append($('<div>', {
+    //                         'class': 'piece white'
+    //                     })).removeClass("empty");
+    //                 }
+    //             self.currentTurn = self.states.player1;
+    //
+    //         },
+    //         // Find piece until matches player's piece? Then flip all opposite stones in btwn
+    //         flipPiece: function () {
+    //             console.log("Flip piece.");
+    //         }
+    //     }
+    // };
+    // this.currentTurn = this.states.player1;
+    // console.log(this.currentTurn);
 }
-
-
-
-// var states = {
-//     player1: function () {
-//
-//     // Check adjacent piece for valid move?
-//     function checkNeighbors() {
-//
-//     }
-//
-//     // Validity = true, place piece
-//     function placePiece() {
-//
-//     }
-//     // Find piece until matches player's piece? Then flip all opposite stones in btwn
-//     function flipPiece() {
-//
-//     }
-//
-// },
-//
-// player2: function () {
-//     function checkNeighbors() {
-//
-//     }
-//
-//     function placePiece() {
-//
-//     }
-//
-//     function flipPiece() {
-//
-//     }
-//
-// }
-// };
