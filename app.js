13d7a2965bf4e464f90627dcd8e8a98c8dc54726
unleashalicia <alicia.adams.evans@gmail.com>
2017-11-16 14:15:10 -0800

    Not sure if I did anything here beside deleting comments.

--- app.js
+++ app.js
@@ -269,7 +269,6 @@ function Othello() {
             }
         }
     }
-}
 
 
 
@@ -277,52 +276,7 @@ function Othello() {
 
 
 
-// this.states = {
-//     player1: {
-//         name: "player 1",
-//         // Check adjacent piece for valid move?
-//         checkNeighbors: function () {
-//             console.log("Check Neighbors.");
-//         },
-//         // Validity = true, place piece
-//         placePiece: function () {
-//                 if ($(event.target).hasClass("empty")) {
-//                     $(event.target).append($('<div>', {
-//                         'class': 'piece black'
-//                     })).removeClass("empty");
-//
-//                 }
-//             self.currentTurn = self.states.player2;
-//             },
-//         // Find piece until matches player's piece? Then flip all opposite stones in btwn
-//         flipPiece: function () {
-//             console.log("Flip piece.")
-//         }
-//     },
-//     player2: {
-//         name: "player 2",
-//         // Check adjacent piece for valid move?
-//         checkNeighbors: function () {
-//             console.log("Check Neighbors.");
-//         },
-//         // Validity = true, place piece
-//         placePiece: function () {
-//                 if ($(event.target).hasClass("empty")) {
-//                     $(event.target).append($('<div>', {
-//                         'class': 'piece white'
-//                     })).removeClass("empty");
-//                 }
-//             self.currentTurn = self.states.player1;
-//
-//         },
-//         // Find piece until matches player's piece? Then flip all opposite stones in btwn
-//         flipPiece: function () {
-//             console.log("Flip piece.");
-//         }
-//     }
-// };
-// this.currentTurn = this.states.player1;
-// console.log(this.currentTurn);
+
 
 
 
